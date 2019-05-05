3434b57539789aea39c884e2a1852e9d84650224
Angel Fernandez <mike.ferndz@outlook.com>
2019-05-05 18:24:33 -0400

    First Commit

---
+++ Arena.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+public class Arena extends Moves {
+
+    public static Entity p1;
+    public static Entity p2;
+    public static String output = "";
+
+    public Arena(Entity A, Entity B) {
+        setPriority(A, B);
+    }
+
+    public String run(int Rounds) {
+        output = "";
+        for (int i = 0; i < Rounds; i++) {
+            if (p1.life == 0 || p2.life == 0) {
+                i = Rounds; //breaks for loop
+            } else {
+                output += "TURN :" + i + "\n";
+                selectAttack(p1, p2);
+                if (p2.life > 0) {
+                    selectAttack(p2, p1);
+                    if(i == 3){
+                        p1.stamina = 3;
+                        p2.stamina = 3;
+                    }else if(i == 5){
+                        p1.stamina = 4;
+                        p2.stamina = 4;
+                    }
+                }
+                output += "End Turn \n";
+                output += p1.name + " " + p1.life + "/" + p1.lifelimit + "\n";
+                output += p2.name + " " + p2.life + "/" + p2.lifelimit + "\n";
+                output += "========================\n";
+            }
+        }
+        return output;
+    }
+
+    public static void setPriority(Entity A, Entity B) {
+        if (A.speed > B.speed) {
+            p1 = A;
+            p2 = B;
+
+        } else {
+            p1 = B;
+            p2 = A;
+        }
+    }
+
+    public static void selectAttack(Entity A, Entity B) {
+
+        switch (A.chooseAttack()) {
+            case 1:
+                punch(A, B);
+                break;
+            case 2:
+                kick(A, B);
+                break;
+            case 3:
+                Kamehameha(A, B);
+                break;
+            case 4:
+                GalickGun(A, B);
+                break;
+            case 5:
+                SpiritBomb(A, B);
+                break;
+            case 6:
+                FinalFlash(A, B);
+                break;
+            case 7:
+                Heal(A);
+                break;
+        }
+    }
+}
---
+++ Moves.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+import java.util.Random;
+
+public class Moves {
+    
+    public static void punch(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(3) + 1;
+        double power = A.str * 1.1;
+        Arena.output += A.name + " Threw a Punch \n";
+        if (i == 1) {
+             
+            Arena.output += B.name + " Blocked Punch!\n";
+        } else {
+            B.Damage(power);
+            Arena.output += B.name + " Hit -" + power + "\n";
+        }
+    }
+
+    public static void kick(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(3) + 1;
+        double power = A.str * 1.3;
+
+        Arena.output += A.name + " used Kick -" + power + "\n";
+
+        if (i == 1) {
+            Arena.output += B.name + " Blocked Kick!\n";
+        } else {
+            B.Damage(power);
+            
+        }
+    }
+    
+    public static void Kamehameha(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(5) + 1;
+        double power = A.str * 2.5;
+
+        Arena.output += A.name + " used Kamehameha! -" + power + "\n";
+
+        if (i == 1) {
+            Arena.output += B.name + " Evaded Kamehameha!\n";
+        } else {
+            B.Damage(power);
+        }
+    }
+
+    public static void GalickGun(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(5) + 1;
+        double power = A.str * 2.5;
+
+        Arena.output += A.name + " Galick Gun! -" + power + "\n";
+
+        if (i == 1) {
+            Arena.output += B.name + " Evaded GalickGun!\n";
+        } else {
+            B.Damage(power);
+        }
+    }
+    
+    public static void SpiritBomb(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(20) + 1;
+        double power = A.str * 5;
+
+        Arena.output += A.name + " used SpiritBomb! -" + power + "\n";
+
+        if (i == 1) {
+            Arena.output += B.name + " Evaded SpiritBomb!\n";
+        } else if (i == 20) {
+            Arena.output += "Holy Fuck " + B.name + " Pushed Back The SpiritBomb!!!!\n";
+            A.Damage(power);
+        } else {
+            B.Damage(power);
+        }
+    }
+
+    public static void FinalFlash(Entity A, Entity B) {
+        Random rand = new Random();
+        int i = rand.nextInt(10) + 1;
+        double power = A.str * 4;
+
+        Arena.output += A.name + " used FinalFlash! -" + power + "\n";
+
+        if (i == 1) {
+            Arena.output += B.name + " Evaded FinalFlash!\n";
+        } else if (i == 10) {
+            Arena.output += B.name + " Countered FinalFlash!!!\n";
+            A.Damage(power);
+        } else {
+            B.Damage(power);
+        }
+    }
+
+    public static void Heal(Entity A) {
+        A.addLife(20);
+    }
+
+}
